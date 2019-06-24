6c91567c1f3af3e6a24fff4f386fd532a8d6ad22
Guillaume Pouyat <guiforge@gmail.com>
2019-06-24 17:34:48 +0200

    First commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Welcome on Super File transfert !</h1>
+    <p>
+      <b-button
+        pill
+        variant="primary">
+        <router-link
+          style="color:white"
+          class="btn"
+          to="/upload">
+          Upload your files
+        </router-link>
+      </b-button>
+    </p>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'HelloWorld',
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/Upload.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Uploads your files!</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Upload',
+};
+</script>
